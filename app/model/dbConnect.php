1e1f5b99b6dfc6b35596e2f7fcc528c23bfbff89
Olger Castro <olger60019castro@gmail.com>
2021-07-09 13:19:02 -0500

    RESPALDADO 09072021 PRIMER LANZAMIENTO 1

--- app/model/dbConnect.php
+++ app/model/dbConnect.php
@@ -1,9 +1,13 @@
 <?php
-class Conexion{
-	static public function conectar(){
-		$link = new PDO("mysql:host=localhost;dbname=acpsy",
-			            "root",
-			            "");
+class Conexion
+{
+	static public function conectar()
+	{
+		$link = new PDO(
+			"mysql:host=localhost;dbname=acpsy",
+			"adm-acpsy",
+			'ldX7aMq4a$TS$7aBTKuD'
+		);
 		$link->exec("set names utf8");
 		return $link;
 	}
