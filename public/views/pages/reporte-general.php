285acd121ea34c7c88a5db83080c894c264db715
Olger Castro <olger60019castro@gmail.com>
2021-07-09 14:56:58 -0500

    RESPALDADO 09072021 PRIMER LANZAMIENTO 3

--- public/views/pages/reporte-general.php
+++ public/views/pages/reporte-general.php
@@ -28,28 +28,27 @@ if ($_SESSION["loginPerfil"] == 3) {
         <h3 class="card-title">Módulo Reporte General &nbsp;<i class="fas fa-chart-pie"></i></h3>
       </div>
       <div class="card-body">
-        <input type="hidden" value="<?php echo $_SESSION["loginId"]; ?>" id="idAtencionUsuario">
-        <button type="btn" class="btn btn-danger"><i class="fas fa-file-excel"></i> Auditoría Atenciones
-        </button>
-        <button type="btn" class="btn btn-secondary"><i class="fas fa-file-excel"></i> Auditoría Familiares
-        </button>
-        <button type="btn" class="ml-2 btn btn-success float-right" id="deshacer-filtro-RG"><i class="fas fa-undo-alt"></i> Deshacer filtro
-        </button>
-        <button type="button" class="btn btn-default float-right" id="rango-rg">
-          <span>
-            <i class="far fa-calendar-alt"></i>
-            <?php
-            if (isset($_GET["fechaInicialRG"])) {
-
-              echo $_GET["fechaInicialRG"] . " - " . $_GET["fechaFinalRG"];
-            } else {
-
-              echo 'Seleccione Rango de fecha';
-            }
-            ?>
-          </span>
-          <i class="fas fa-caret-down"></i>
-        </button>
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-7 col-xl-7">
+            <div class="form-group">
+              <label>Selecciona un rango de fecha:</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="far fa-calendar-alt"></i>
+                  </span>
+                </div>
+                <input type="text" class="form-control float-right" id="rango-rg" readonly>
+                <button type="btn" class="ml-2 btn bg-success pull-right" id="deshacer-filtro-RG"><i class="fas fa-undo-alt"></i> Deshacer filtro
+                </button>
+                <button type="btn" class="btn btn-secondary float-right ml-2"><i class="fas fa-file-excel"></i> Auditoría Familiares
+                </button>
+                <button type="btn" class="btn btn-danger float-right ml-2"><i class="fas fa-file-excel"></i> Auditoría Atenciones
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
