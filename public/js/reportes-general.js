245fe5067ce63d9b51dcfcf146a7e0e7c7287cc0
Olger Castro <olger60019castro@gmail.com>
2021-07-12 08:03:06 -0500

    12072021

--- public/js/reportes-general.js
+++ public/js/reportes-general.js
@@ -3,15 +3,15 @@ $("#deshacer-filtro-RG").on("click", function () {
     window.location = "reporte-general";
 });
 
-$("#rango-rg").daterangepicker({
-    opens: "left",
+$("input[name='rango-rg']").daterangepicker({
+    opens: 'left',
     maxSpan: {
         days: 30,
     },
     startDate: moment(),
     endDate: moment(),
     locale: {
-        format: "DD-MM-YYYY",
+        format: "DD/MM/YYYY",
         separator: " hasta ",
         applyLabel: "Aplicar",
         cancelLabel: "Cancelar",
@@ -36,8 +36,149 @@ $("#rango-rg").daterangepicker({
         ],
         firstDay: 1,
     },
+}, function (start, end) {
+    let inicio = start.format('YYYY-MM-DD');
+    let fin = end.format('YYYY-MM-DD')
+
+    $(".rg1").attr("href", "public/views/docs/rp-audfamiliares.php?reporte=reporte&inicio=" + inicio + "&fin=" + fin);
+    $(".rg2").attr("href", "public/views/docs/rp-audatenciones.php?reporte=reporte&inicio=" + inicio + "&fin=" + fin);
+});
+
+
+cargarAteRegAnu();
+
+function cargarAteRegAnu() {
+    var ticksStyle = {
+        fontColor: '#495057',
+        fontStyle: 'bold'
+    }
+    var mode = 'index'
+    var intersect = true
+
+    var $salesChart = $('#sales-chart')
+    // eslint-disable-next-line no-unused-vars
+    var salesChart = new Chart($salesChart, {
+        type: 'bar',
+        data: {
+            labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+            datasets: [
+                {
+                    backgroundColor: '#007bff',
+                    borderColor: '#007bff',
+                    data: [1000, 2000, 3000, 2500, 2700, 2500, 3000]
+                },
+                {
+                    backgroundColor: '#ced4da',
+                    borderColor: '#ced4da',
+                    data: [700, 1700, 2700, 2000, 1800, 1500, 2000]
+                }
+            ]
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                mode: mode,
+                intersect: intersect
+            },
+            hover: {
+                mode: mode,
+                intersect: intersect
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    // display: false,
+                    gridLines: {
+                        display: true,
+                        lineWidth: '4px',
+                        color: 'rgba(0, 0, 0, .2)',
+                        zeroLineColor: 'transparent'
+                    },
+                    ticks: $.extend({
+                        beginAtZero: true,
+                    }, ticksStyle)
+                }],
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: ticksStyle
+                }]
+            }
+        }
+    })
+
+    var $visitorsChart = $('#visitors-chart')
+    // eslint-disable-next-line no-unused-vars
+    var visitorsChart = new Chart($visitorsChart, {
+        data: {
+            labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
+            datasets: [{
+                type: 'line',
+                data: [100, 120, 170, 167, 180, 177, 160],
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                pointBorderColor: '#007bff',
+                pointBackgroundColor: '#007bff',
+                fill: false,
+                pointHoverBackgroundColor: '#007bff',
+                pointHoverBorderColor: '#007bff'
+            },
+            {
+                type: 'line',
+                data: [60, 80, 70, 67, 80, 77, 100],
+                backgroundColor: 'tansparent',
+                borderColor: '#ced4da',
+                pointBorderColor: '#ced4da',
+                pointBackgroundColor: '#ced4da',
+                fill: false,
+                pointHoverBackgroundColor: '#ced4da',
+                pointHoverBorderColor: '#ced4da'
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                mode: mode,
+                intersect: intersect
+            },
+            hover: {
+                mode: mode,
+                intersect: intersect
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    // display: false,
+                    gridLines: {
+                        display: true,
+                        lineWidth: '4px',
+                        color: 'rgba(0, 0, 0, .2)',
+                        zeroLineColor: 'transparent'
+                    },
+                    ticks: $.extend({
+                        beginAtZero: true,
+                        suggestedMax: 200
+                    }, ticksStyle)
+                }],
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: ticksStyle
+                }]
+            }
+        }
+    })
 }
-);
+// Evento captura rango
+// Evento captura rango
 // $("#rango-rg").daterangepicker({
 
 //     ranges: {
--- public/views/pages/reporte-general.php
+++ public/views/pages/reporte-general.php
@@ -26,7 +26,24 @@ if ($_SESSION["loginPerfil"] == 3) {
     <div class="card">
       <div class="card-header">
         <h3 class="card-title">Módulo Reporte General &nbsp;<i class="fas fa-chart-pie"></i></h3>
+        <div class="card-tools">
+          <a href="public/views/docs/rp-audatenciones.php?reporte=reporte" class="rg2">
+            <button type="btn" class="btn btn-danger"><i class="fas fa-file-excel"></i> Auditoría Atenciones
+            </button>
+          </a>
+          <a href="public/views/docs/rp-audfamiliares.php?reporte=reporte" class="rg1">
+            <button type="btn" class="btn btn-secondary"><i class="fas fa-file-excel"></i> Auditoría Familiares
+            </button>
+          </a>
+          <!-- <button type="button" class="btn btn-tool" data-card-widget="collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove">
+            <i class="fas fa-times"></i>
+          </button> -->
+        </div>
       </div>
+
       <div class="card-body">
         <div class="row">
           <div class="col-12 col-sm-12 col-md-12 col-lg-7 col-xl-7">
@@ -38,15 +55,100 @@ if ($_SESSION["loginPerfil"] == 3) {
                     <i class="far fa-calendar-alt"></i>
                   </span>
                 </div>
-                <input type="text" class="form-control float-right" id="rango-rg" readonly>
+                <input type="text" class="form-control float-right" id="rango-rg" name="rango-rg" readonly>
                 <button type="btn" class="ml-2 btn bg-success pull-right" id="deshacer-filtro-RG"><i class="fas fa-undo-alt"></i> Deshacer filtro
-                </button>
-                <button type="btn" class="btn btn-secondary float-right ml-2"><i class="fas fa-file-excel"></i> Auditoría Familiares
-                </button>
-                <button type="btn" class="btn btn-danger float-right ml-2"><i class="fas fa-file-excel"></i> Auditoría Atenciones
                 </button>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card card-gray">
+          <div class="card-header">
+            <h3 class="card-title font-weight-bold">Atenciones Registradas y Anuladas</h3>
+          </div>
+          <div class="card-body">
+            <div class="position-relative mb-4">
+              <canvas id="sales-chart" height="200"></canvas>
+            </div>
+
+            <div class="d-flex flex-row justify-content-end">
+              <span class="mr-2">
+                <i class="fas fa-clipboard-list text-primary"></i>
+                <i class="fas fa-square text-primary"></i> Registradas
+              </span>
+              <span>
+                <i class="fas fa-ban text-gray"></i>
+                <i class="fas fa-square text-gray"></i> Anuladas
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card card-gray">
+          <div class="card-header">
+            <h3 class="card-title font-weight-bold">Familiares Registradas y Eliminados</h3>
+          </div>
+          <div class="card-body">
+            <!-- <div class="chart">
+              <canvas id="rgg1" width="400" height="400"></canvas>
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card card-gray">
+          <div class="card-header">
+            <h3 class="card-title font-weight-bold">Seguimientos Registrados y Anulados</h3>
+          </div>
+          <div class="card-body">
+            <!-- <div class="chart">
+              <canvas id="rgg1" width="400" height="400"></canvas>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card card-gray">
+          <div class="card-header">
+            <h3 class="card-title font-weight-bold">Tipo de Parentesco</h3>
+          </div>
+          <div class="card-body">
+            <!-- <div class="chart">
+              <canvas id="rgg1" width="400" height="400"></canvas>
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card card-gray">
+          <div class="card-header">
+            <h3 class="card-title font-weight-bold">Edad</h3>
+          </div>
+          <div class="card-body">
+            <!-- <div class="chart">
+              <canvas id="rgg1" width="400" height="400"></canvas>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card card-gray">
+          <div class="card-header">
+            <h3 class="card-title font-weight-bold">Sexo</h3>
+          </div>
+          <div class="card-body">
+            <!-- <div class="chart">
+              <canvas id="rgg1" width="400" height="400"></canvas>
+            </div> -->
           </div>
         </div>
       </div>
